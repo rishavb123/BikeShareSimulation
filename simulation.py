1121d53eb98cad301a5e7c6e54b890e33a999018
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-04-25 22:33:49 -0400

    finished loading in config

--- simulation.py
+++ simulation.py
@@ -18,25 +18,50 @@ class Simulation:
 
         self.k = bikes_per_station
 
-        self.stations, self.p_i = self.load_start_probabilities(
-            start_station_probs_file
+        self.stations, self.p = self.load_start_probabilities(
+            start_station_probs_file=start_station_probs_file
         )
         self.m = len(self.stations)
+        self.stations_mapping = self.build_stations_mapping(self.stations)
 
+        self.q = self.load_trip_stats(trip_stats_file=trip_stats_file)
 
         self.end_time = end_time
 
     def load_start_probabilities(self, start_station_probs_file):
         stations = []
-        p_i = []
+        p = []
         with open(start_station_probs_file) as f:
             reader = csv.reader(f)
             _ = next(reader)
             for row in reader:
-                name, p = row
+                name, prob = row
                 stations.append(name)
-                p_i.append(float(p))
-        return stations, p_i
+                p.append(float(prob))
+        return np.array(stations), np.array(p)
     
+    def build_stations_mapping(self, stations):
+        mapping = {}
+        for i in range(len(stations)):
+            mapping[stations[i]] = i
+        return mapping
+    
+    def load_trip_stats(self, trip_stats_file):
+        q = np.zeros((self.m, self.m))
+        with open(trip_stats_file) as f:
+            reader = csv.reader(f)
+            _ = next(reader)
+            for row in reader:
+                start, end, count, _, _ = row
+                i = self.stations_mapping[start]
+                j = self.stations_mapping[end]
+                q[i, j] += int(count)
+        q_sum = np.sum(q, axis=1).reshape(-1, 1)
+        q = q / q_sum
+        return q
+
+
     def run(self):
-        print(self.m)
+        print(self.p)
+        print(self.q)
+        print(self.stations)

816be9d2259d310fdebb237dfa263747de5bb1f0
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-04-26 17:01:23 -0400

    pretty printing json stats

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from args_utils import get_args
 from config import make_parser
 from simulation import Simulation
@@ -5,7 +7,7 @@ from simulation import Simulation
 def main():
     args = get_args(make_parser(), configs_root="./configs/")
     s = Simulation(**vars(args))
-    s.run()
+    print(json.dumps(s.run(), indent=4))
 
 if __name__ == "__main__":
     main()
